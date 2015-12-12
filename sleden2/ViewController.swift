354348303f3e58aa1811376c36ef38d3186fb8bc
Daniel Alvestad <Daniel@daniels-macbook.local>
2015-12-12 17:30:07 +0100

    Initial Commit

---
+++ sleden2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sleden2
+//
+//  Created by Daniel Alvestad on 12/12/15.
+//  Copyright © 2015 Daniel Alvestad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
