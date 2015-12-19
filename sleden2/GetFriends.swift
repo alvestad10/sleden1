02cf689f9515f725eb5b0a6b09b89734b74cc311
alvestad10 <Daniel@Daniels-Macbook.local>
2015-12-19 21:32:29 +0100

    Tabellen i myFriends viser nå brukernavn
    Brukernavnene viser nå ved å hente de fra objectID til de som er
    venner. Bruker funksjonen findUsername

--- sleden2/GetFriends.swift
+++ sleden2/GetFriends.swift
@@ -33,36 +33,75 @@ class GetFriends {
     func findFriends(view: myFrindsViewController) {
         
         // Make call to user tabel i database and fetch friends
-        let queryFriends = PFQuery(className: "Friends")
-        queryFriends.whereKey("user1", equalTo: (PFUser.currentUser()?.objectId)!)
-        //queryFriends.whereKey("user2", equalTo: (PFUser.currentUser()?.objectId)!)
-        queryFriends.findObjectsInBackgroundWithBlock({ (objects: [PFObject]?, error: NSError?) -> Void in
+        let queryFriends1 = PFQuery(className: "Friends")
+        let queryFriends2 = PFQuery(className: "Friends")
+        queryFriends1.whereKey("user1", equalTo: (PFUser.currentUser()?.objectId)!)
+        queryFriends2.whereKey("user2", equalTo: (PFUser.currentUser()?.objectId)!)
+        
+        queryFriends1.findObjectsInBackgroundWithBlock({ (objects: [PFObject]?, error: NSError?) -> Void in
             
             if error == nil {
                 for object in objects! {
                     print("Found user")
-                    if (object["user1"] as? String == PFUser.currentUser()?.objectId){
-                        
-                        let newFriendsInArray = User(userID: object["user2"] as! String, isFriend: userRelation.Friend)
-                        self.myFriends.append(newFriendsInArray)
-                        print(2)
-                    } else {
-                        let newFriendsInArray = User(userID: object["user1"] as! String, isFriend: userRelation.Friend)
-                        self.myFriends.append(newFriendsInArray)
-                        print(1)
-                    }
+                    let newFriendsInArray = User(userID: object["user2"] as! String, isFriend: userRelation.Friend)
+                    self.myFriends.append(newFriendsInArray)
+                    self.findUsername(self.myFriends.count-1, view: view)
+                    print(2)
                     
                 }
-                view.updateTableFromModule()
+                
                 
             } else {
                 print(error)
             }
         })
+        queryFriends2.findObjectsInBackgroundWithBlock({ (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                for object in objects! {
+                    print("Found user")
+                    let newFriendsInArray = User(userID: object["user1"] as! String, isFriend: userRelation.Friend)
+                    self.myFriends.append(newFriendsInArray)
+                    self.findUsername(self.myFriends.count-1, view: view)
+                    print(2)
+                    
+                }
+                
+                
+            } else {
+                print(error)
+            }
+        })
+
     }
     
     
-
+    func findUsername(userIndex: Int, view: myFrindsViewController){
+        
+        let queryUser = PFUser.query()
+        queryUser?.whereKey("objectId", equalTo: self.myFriends[userIndex].userID)
+        queryUser?.findObjectsInBackgroundWithBlock({ (users: [PFObject]?, error: NSError?) -> Void in
+            
+            
+            if error == nil {
+                
+                if let isUsers = users {
+                    for userDB in isUsers {
+                        self.myFriends[userIndex].username = userDB["username"] as? String
+                        print(self.myFriends[userIndex].username!)
+                    }
+                    
+                }
+                view.updateTableFromModule()
+                
+            }
+            
+            
+            
+        })
+        
+        
+    }
     
     
     
